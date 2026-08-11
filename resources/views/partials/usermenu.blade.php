ac7f77fcbf394a524d09a58f7cd84755c17e6397
Ruan Pépe <ruanpepe@gmail.com>
2026-08-11 13:34:58 -0300

    fix(layout): Refactor user menu to use config for profile URL

--- resources/views/partials/usermenu.blade.php
+++ resources/views/partials/usermenu.blade.php
@@ -1,10 +1,13 @@
 @php
     $user = auth()->user();
     $name = $user->name ?? ($user->email ?? 'Guest');
-    $avatar = (config('adminlte.usermenu_image') && ! empty($user?->profile_photo_url))
+    $avatar = !empty($user?->profile_photo_url)
         ? $user->profile_photo_url
         : asset('vendor/adminlte/img/user2-160x160.jpg');
     $memberSince = $user?->created_at ? $user->created_at->format('M. Y') : null;
+    $profileUrl = config('adminlte.usermenu_profile_url', false) === true
+        ? 'admin/profile'
+        : config('adminlte.usermenu_profile_url');
 @endphp
 <li class="nav-item dropdown user-menu">
     <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
@@ -13,13 +16,23 @@
     </a>
     <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
         {{-- Header --}}
-        <li class="user-header text-bg-primary">
-            <img src="{{ $avatar }}" class="rounded-circle shadow" alt="{{ $name }}" width="90" height="90">
-            <p>
-                {{ $name }}
-                @if ($memberSince)<small>{{ __('adminlte.member_since') }} {{ $memberSince }}</small>@endif
-            </p>
-        </li>
+        
+        @if(config('adminlte.usermenu_header'))
+            <li class="user-header {{ config('adminlte.usermenu_header_class', 'text-bg-primary') }}
+                @if(!config('adminlte.usermenu_image')) h-auto @endif" style="min-height: 0px;">
+                
+                @if(config('adminlte.usermenu_image'))
+                    <img src="{{ $avatar }}" class="rounded-circle shadow" alt="{{ $name }}" width="90" height="90">
+                @endif
+                <p>
+                    {{ $name }}
+                    @if(config('adminlte.usermenu_desc'))
+                        @if ($memberSince)<small>{{ __('adminlte.member_since') }} {{ $memberSince }}</small>@endif
+                    @endif
+                </p>
+            </li>
+        @endif
+        
         {{-- Body --}}
         <li class="user-body">
             <div class="row">
@@ -30,10 +43,13 @@
         </li>
         {{-- Footer --}}
         <li class="user-footer">
-            <a href="{{ url(config('adminlte.usermenu_profile_url') ?: 'admin/profile') }}" class="btn btn-outline-secondary">
-                {{ __('adminlte.profile') }}
-            </a>
-            <a href="#" class="btn btn-outline-danger float-end"
+            @if (config('adminlte.usermenu_profile_url'))
+                <a href="{{ $profileUrl }}" class="btn btn-outline-secondary">
+                    {{ __('adminlte.profile') }}
+                </a>
+            @endif
+            
+            <a href="#" class="btn btn-outline-danger float-end @if(!config('adminlte.usermenu_profile_url')) w-100 @endif"
                onclick="event.preventDefault(); document.getElementById('adminlte-logout-form').submit();">
                 {{ __('adminlte.sign_out') }}
             </a>
